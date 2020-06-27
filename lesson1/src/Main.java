c4c62ef636e1930f11f12c3b5c2568a95d8ff5e9
Semyon <khalyavin.semyon@yandex.ru>
2020-06-27 17:21:52 +0300

    Сделал первое и второе задание

--- lesson1/src/Main.java
+++ lesson1/src/Main.java
@@ -1,6 +1,15 @@
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        String[] input = {"мама", "папа", "брат", "сестра"};
+	    MyArray<String> arr = new MyArray<>(input);
+	    arr.printArray();
+	    arr.swap(1,2);
+	    arr.printArray();
+	    ArrayList<String> list = new ArrayList<>();
+	    list = arr.toArrayList();
+	    System.out.println(list.getClass());
     }
 }
